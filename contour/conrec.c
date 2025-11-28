0ecd07d757f18395eccd04044670f50ad5e20c4b
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-11-28 22:29:10 +0100

    modernized

--- contour/conrec.c
+++ contour/conrec.c
@@ -11,27 +11,11 @@
  *
  */
 
-#ifdef __GNUC__
-# ifndef MAX
-# define MAX(a,b)                               \
-  ({ __typeof__ (a) _a = (a);                   \
-    __typeof__ (b) _b = (b);                    \
-    _a > _b ? _a : _b; })
-# endif
-
-# ifndef MIN
-# define MIN(a,b)                               \
-  ({ __typeof__ (a) _a = (a);                   \
-    __typeof__ (b) _b = (b);                    \
-    _a < _b ? _a : _b; })
-# endif
-#else
-# ifndef MIN
-#  define MIN(a,b) (((a)<(b))?(a):(b))
-# endif
-# ifndef MAX
-#  define MAX(a,b) (((a)>(b))?(a):(b))
-# endif
+#ifndef MIN
+# define MIN(a,b) (((a)<(b))?(a):(b))
+#endif
+#ifndef MAX
+# define MAX(a,b) (((a)>(b))?(a):(b))
 #endif
 /*
    Derivation from the fortran version of CONREC by Paul Bourke
--- setup.py
+++ setup.py
@@ -1,16 +1,43 @@
 #!/usr/bin/env python
+"""
+Setup script for swig_contour Python extension.
+
+Usage:
+    python setup.py build_ext --inplace
+"""
+import os
+import shutil
 import numpy
-from distutils.core import setup, Extension
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-setup(name="swig_contour",
-      py_modules=["swig_contour"],
-      ext_modules=[Extension("contour/_swig_contour",
-                     ["contour/swig_contour.i", "contour/contour.cpp", "contour/conrec.c"],
-                     include_dirs = [numpy.get_include(), '.'],
-                     swig_opts=['-c++', '-I.'],
-                  )]
 
+class BuildExtWithCopy(build_ext):
+    """Custom build_ext that copies SWIG-generated .py to package root."""
+
+    def run(self):
+        super().run()
+        # Copy SWIG-generated wrapper to root for proper import
+        src = os.path.join(os.path.dirname(__file__), 'contour', 'swig_contour.py')
+        dst = os.path.join(os.path.dirname(__file__), 'swig_contour.py')
+        if os.path.exists(src):
+            shutil.copy2(src, dst)
+            print(f"Copied {src} -> {dst}")
+
+
+setup(
+    ext_modules=[
+        Extension(
+            name='_swig_contour',
+            sources=[
+                'contour/swig_contour.i',
+                'contour/contour.cpp',
+                'contour/conrec.c',
+            ],
+            include_dirs=[numpy.get_include(), '.'],
+            swig_opts=['-c++', '-I.'],
+            language='c++',
+        )
+    ],
+    cmdclass={'build_ext': BuildExtWithCopy},
 )
-
-
-
